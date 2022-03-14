b65df4a602ed2529d78aae5027e40c2c5c0096cc
KariukiD <kariukindiho@gmail.com>
2022-03-14 12:39:55 -0700

    Week 3 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+
+ * main - Print the alphabet in lowercase letters, except for e & q
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+	char a;
+
+	
+
+	for (a = 'a'; a <= 'z'; a++)
+
+	{
+
+		if (a != 'e' && a != 'q')
+
+		putchar(a);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
