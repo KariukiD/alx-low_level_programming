f47005b6b0bd6587cfc3d59e9147f20ea26ff3af
KariukiD <kariukindiho@gmail.com>
2022-03-14 12:49:21 -0700

    Week 3 commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+
+ * main - Print lowercase alphabet in reverse
+
+ *
+
+ * Return: 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+	char a;
+
+
+
+	for (a = 'z'; a >= 'a'; a--)
+
+	{
+
+		putchar(a);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
