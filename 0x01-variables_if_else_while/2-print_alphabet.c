1bf0ccdb62ca61f14e62edb64e472e53d4d038f5
KariukiD <kariukindiho@gmail.com>
2022-03-14 12:31:54 -0700

    Week 3 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+
+ * main - Alphabet in Lowercase
+
+ *
+
+ * Return: 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+	char a;
+
+
+
+	for (a = 'a'; a <= 'z'; a++)
+
+	{
+
+		putchar(a);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
