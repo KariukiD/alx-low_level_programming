03ec7203b10d450922862e5760f3e909a105e0e3
KariukiD <kariukindiho@gmail.com>
2022-03-14 12:55:04 -0700

    Week 3 commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+
+ * main - Prints all possible combos of single-digit numb
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+
+
+	for (i = 48; i < 58; i++)
+
+	{
+
+		putchar(i);
+
+		if (i != 57)
+
+		{
+
+			putchar(',');
+
+			putchar(' ');
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
