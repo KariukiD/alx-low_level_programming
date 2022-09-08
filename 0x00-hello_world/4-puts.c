2cf047e1c40d22bf1b3206691bf9fca832394dc7
KariukiD <kariukindiho@gmail.com>
2022-09-08 08:17:02 -0700

    use puts as printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
-
-* main - entry point
-*
-* Return: always 0 (Sucess)
-*/
+ * main -Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
