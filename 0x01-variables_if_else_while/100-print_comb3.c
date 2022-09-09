1f85d1ce2306ba9c4d0fcdc39236c92ce8da54cc
KariukiD <kariukindiho@gmail.com>
2022-09-09 04:46:02 -0700

    Write a program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,65 +1,30 @@
 #include <stdio.h>
-
 #include <unistd.h>
-
 /**
-
  * main - All possible combinations of 2 digits
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-
 {
-
 	int a, b;
 
-
-
 	for (a = '0'; a <= '9'; a++)
-
 	{
-
 		for (b = '0'; b <= '9'; b++)
-
 		{
-
 			if (a < b)
-
 			{
-
-
-
 				putchar(a);
-
 				putchar(b);
-
-
-
 				if (a != '8' || (a == '8' && b != '9'))
-
 				{
-
 					putchar(',');
-
 					putchar(' ');
-
 				}
-
 			}
-
-
-
 		}
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
