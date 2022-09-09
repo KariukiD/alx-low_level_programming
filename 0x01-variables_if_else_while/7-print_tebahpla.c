c20c533d91c15fa4fedd363fc4e4e61ff6a2a7ae
KariukiD <kariukindiho@gmail.com>
2022-09-09 04:36:47 -0700

    Write a program that prints the lowercase alphabet in reverse, followed by a new line use putchar max twice

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
-
 /**
-
  * main - Print lowercase alphabet in reverse
-
  *
-
  * Return: 0 (Success)
-
  */
-
 int main(void)
-
 {
-
 	char a;
 
-
-
 	for (a = 'z'; a >= 'a'; a--)
-
 	{
-
 		putchar(a);
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
