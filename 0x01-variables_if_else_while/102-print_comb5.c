6d61795e7ee65da570c4f8687b4c6f4578bc0884
KariukiD <kariukindiho@gmail.com>
2022-09-11 03:03:15 -0700

    two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,12 +10,14 @@ int main(void)
 {
 	int i;
 
-	for (int i = 'o'; i < 100; i++)
+	for (int i = 'o'; i < '99'; i++)
 	{
 		putchar((i/10) + '0');
 		putchar((i/10) + '0');
 	}
-	putchar("\n");
+	putchar(' ')
+	putchar(',')
+	putchar('\n');
 	return (0);
 }
 
