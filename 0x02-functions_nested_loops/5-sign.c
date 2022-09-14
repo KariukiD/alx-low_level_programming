e4bcbb00ebb9169035773001949711e13ed3532e
KariukiD <kariukindiho@gmail.com>
2022-09-14 09:07:58 -0700

    input= or < or > 0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,30 @@
 #include "main.h"
-
 /**
- * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ * print_sign - Determines if the input number
+ * greater, equal or less than zero
  *
- *@n: the int to print
- * Return: Always 0.
+ * @n: The input number as an intergr.
+ *
+ * Return: 1 is greater than zero. 0 is zero.
+ * -1 is less than zero.
  */
-int print_sign(int n)
+int print_sign(int_n)
 {
-if (n > 0)
-{
-_putchar ('+');
-return (1);
+	if (n > 0)
+	{
+		_putchar(43);
+		return(1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
 }
 
-else if (n == 0)
-{
-_putchar ('0');
-return (0);
-}
-
-else
-{
-_putchar ('-');
-return (-1);
-}
-
-}
