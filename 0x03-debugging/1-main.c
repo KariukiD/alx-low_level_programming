3fefb555caa7b2f2ce33881b9e28d2976127aba1
KariukiD <kariukindiho@gmail.com>
2022-09-14 13:51:35 -0700

    loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,10 +15,10 @@ int main(void)
 	/**
 	*while (i < 10)
 	*{
-	*putchar(i);
+		*putchar(i);
 	*}
 	*/
-	printf(Infinite loop avoided! \\o/\n"0;
+	printf(Infinite loop avoided! \\o/\n";
 
 	return (0);
 }
