e6c62c8ecd3b4e00ee9f572a45e9c7f35fced1c4
KariukiD <kariukindiho@gmail.com>
2022-09-14 08:01:49 -0700

    print alphabet 10 time

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,24 @@
 #include "main.h"
-
 /**
- * print_alphabet_x10 - print alphabet
+ *void print_alphabet_x10 
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-int alphabet;
-int count;
+	int alphabet;
+	int count;
 
+	count = 0
+		while (count < 10)
+		{
+			for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			{
+				_putchar(alphabet);
+			}
 
-count = 0;
-while (count < 10)
-{
-for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
-}
-
-count++;
-_putchar('\n');
-}
-
+			count++;
+			_putchar('\n');
+		}
 
 }
