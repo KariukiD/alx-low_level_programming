8a2a34ec631ce5ce4f1b9829317d06ac19410576
KariukiD <kariukindiho@gmail.com>
2022-09-14 07:00:21 -0700

    pcommit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Return: Always 0
- */
-int main(void);{
+ * Return: Always 0 (Success)
+ */ 
+int main(void);
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
