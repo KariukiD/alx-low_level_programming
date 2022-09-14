a360c1893d1c956cc4269e49360f7742fc50d57b
KariukiD <kariukindiho@gmail.com>
2022-09-14 05:06:01 -0700

    prints_putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,14 @@
 
 /**
  * main - Entry point
+ * Write a program that prints _putchar, followed by a new line.
  *
- * Return: nothing, return void
+ * Returns: 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	putchar("_putchar");
+	putchar("\n");
 	return (0);
 }
+
