53569d123610b4d71ef3974fd56e8933a74881f8
KariukiD <kariukindiho@gmail.com>
2022-09-14 09:59:13 -0700

    compute abs value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,18 +9,11 @@
 int _abs(int n)
 {
 
-	if (n < 0)
-	{
-		return (n * (-1));
-	}
-
-	else if (n == 0)
-	{
-		return (0);
-	}
-
-	else
-	{
+	if (n >= 0)
+	
 		return (n);
-	}
+	else
+	
+		return (n);
 }
+
