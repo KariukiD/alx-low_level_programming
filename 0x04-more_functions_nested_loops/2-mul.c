59d6d60fb837205478bc2d9dc2038d5be4765baa
KariukiD <kariukindiho@gmail.com>
2022-09-15 15:17:21 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * mul -multiplies two intergers.
  * @a: character a.
  * @b: character b.
  * Return: multiplication.
  */
-int mul(int a, int b)
+int main(void)
 {
-	int mult = a * b;
-	return (multi);
+	printf("%d\n", mul(a * b));
+	return (0);
 }
