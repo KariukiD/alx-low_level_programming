b3dd5a19f40458985f023667d15301911d828588
KariukiD <kariukindiho@gmail.com>
2022-09-16 13:21:37 -0700

    print

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print number - prints numbers 0 to 9
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+	for (i >= 0; i <=9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
