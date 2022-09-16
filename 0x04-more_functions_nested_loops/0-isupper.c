c318cb12c436da0a7643c3b25b3ff16dc98fb41c
KariukiD <kariukindiho@gmail.com>
2022-09-16 09:10:38 -0700

    return

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * _isupper - checks for uppercase character
- * @letter: the character to be tracked
+ * @c: the character to be tracked
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
