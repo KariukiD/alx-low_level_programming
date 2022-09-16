d7dd3274f9f7693aa7e437545bc1561c8588d189
KariukiD <kariukindiho@gmail.com>
2022-09-16 14:35:56 -0700

    4

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,7 +1,4 @@
-#include "holberton.h"
-
-
-
+#include "main.h"
 /**
  *
  *  * main - check the code for Holberton School students.
@@ -11,13 +8,10 @@
  *    * Return: Always 0.
  *
  *     */
-
 int main(void)
 
 {
-
 	    print_most_numbers();
 
 	        return (0);
-
 }
