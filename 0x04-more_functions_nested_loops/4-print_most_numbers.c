9ed8b52cec217da1a53670df13f5a649a9c1b214
KariukiD <kariukindiho@gmail.com>
2022-09-16 14:30:03 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - checks numbers
+ * Return: success
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i !=4)
+			_putchar('0' = i);
+	}
+	_putchar('\n';
+}
+			
