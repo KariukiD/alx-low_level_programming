7634ebf1f99a5768328499b17e5b741b328c1c95
KariukiD <kariukindiho@gmail.com>
2022-09-16 15:00:57 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,12 +5,12 @@
  */
 void print_most_numbers(void)
 {
-	int putchar(ch);
+	char ch;
 
 	for (ch = 0; ch <= 9; ch++)
 	{
 		if (ch != 2 && ch !=4)
-			_putchar('0' +  ch);
+			_putchar(ch);
 	}
 	_putchar('\n');
 }
