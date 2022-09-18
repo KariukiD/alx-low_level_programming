950d523c4c37c4bd95cfadab47defcc4908c3687
KariukiD <kariukindiho@gmail.com>
2022-09-18 02:24:19 -0700

    3

--- 0x04-more_functions_nested_loops/3-main.c
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -1,13 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - print_numbers
  * print_numbers - prints numbers
  * Return: Always 0.
  */
-void print_numbers(void);
+void print_numbers(void)
 
-int num()
 {
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
 	    print_numbers();
-	    return (0);
+	}
 }
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,7 +10,8 @@ void print_most_numbers(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
+	{
 		putchar(n);
+	}
 	putchar('\n');
-	return;
 }
