5882e623269598d02d011574e6027444f13b94e0
KariukiD <kariukindiho@gmail.com>
2022-09-25 12:28:31 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,6 +6,16 @@
  */
 void _puts(char *str)
 {
-	*str = "Holberton!";
-	_puts(str);
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
 }
