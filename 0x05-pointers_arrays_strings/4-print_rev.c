50aa9b95beb87b72e341f8fdb3fee7d66593f618
KariukiD <kariukindiho@gmail.com>
2022-09-25 12:40:47 -0700

    print str reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - print code
+ * @s: inout string
+ * Return: no return
+ */
+void print_rev(char *s);
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count ++;
+	}
+
+	for (count--; count >=0; count--)
+	       _putchar(s[count]);
+	_putchar('\n');
+}
