bce8e67467fc1a9faa89d6c9da8175eaf79d59db
KariukiD <kariukindiho@gmail.com>
2022-09-25 12:11:31 -0700

    check

---
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void _puts(char *);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void);
+{
+	char *str;
+
+	str = "Holberton!";
+	_puts(str);
+	return (0);
+}
