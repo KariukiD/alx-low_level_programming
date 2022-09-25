2dfe92baac77925e7a69e8abd3f4971f723f319f
KariukiD <kariukindiho@gmail.com>
2022-09-25 12:45:49 -0700

    print str reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,10 @@ void print_rev(char *s)
 	{
 		if (s[count] == '\0')
 			break;
-		count ++;
+		count++;
 	}
 
-	for (count--; count >=0; count--)
+	for (count--; count >= 0; count--)
 	       _putchar(s[count]);
 	_putchar('\n');
 }
