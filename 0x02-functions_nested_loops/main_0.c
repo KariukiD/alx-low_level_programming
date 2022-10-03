ca068fad61f007243e044011e1aa2e44391cd063
KariukiD <kariukindiho@gmail.com>
2022-10-03 08:39:50 -0700

    not

---
+++ 0x02-functions_nested_loops/main_0.c
@@ -0,0 +1,18 @@
+#include <limits.h>
+#include "main.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = print_last_digit(98);
+	{
+		_putchar('0' + r);
+		_putchar('\n');
+	}
+	return (0);
+}
