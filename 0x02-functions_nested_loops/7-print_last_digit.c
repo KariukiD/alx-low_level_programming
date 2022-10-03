614805aa9db51c056103cf05e348dfc55b01aee3
KariukiD <kariukindiho@gmail.com>
2022-10-03 08:11:35 -0700

    last digit abs value

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,19 +6,8 @@
  * @n: The int to print
  * Return: Always ).
  */
-int _abs(int n)
+int print_last_digit(int n)
 {
-	int l;
-	
-	l = n % 10;
-	if (l < 0)
-	{
-		_putchar(-l + 48);
-		return (-l);
-	}
-	else
-	{
-		_putchar(l + 48);
-		return (l);
-	}
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
 }
