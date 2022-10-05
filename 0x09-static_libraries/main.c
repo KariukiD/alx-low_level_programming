9d2f30c72e6d4b3227c060218967ce03c7bfc9b1
KariukiD <kariukindiho@gmail.com>
2022-10-05 06:30:24 -0700

    static library

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,6 +1,10 @@
 #include "main.h"
+/**
+ * main - main function
+ * _puts - places statement
+ * Return: Always 0
+ */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+	_puts("\"At the end of the day, my...hacker\"\n\t- Kevin Mitnick");
 }
