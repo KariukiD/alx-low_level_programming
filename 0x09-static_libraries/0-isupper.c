ef2d4c74d5376782212b0b18c25724052b2458fc
KariukiD <kariukindiho@gmail.com>
2022-10-05 05:59:52 -0700

    static library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be tracked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
