2227542285077ad5e459f5a708a57d3a72712a87
KariukiD <kariukindiho@gmail.com>
2022-10-05 15:40:18 -0700

    static libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -9,13 +9,13 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do{
+	do {
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
-	}while (*s++);
+	} while (*s++);
 	return (num * sign);
 }
