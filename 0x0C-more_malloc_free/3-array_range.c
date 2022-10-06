0935a46c1c6a9e391fd90dc547e88ffedcdaa4c3
KariukiD <kariukindiho@gmail.com>
2022-10-06 12:09:30 -0700

    more malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,18 +10,14 @@
  */
 int *array_range(int min, int max)
 {
-	int *ar;
-	int i;
-
+	int *p, i;
 	if (min > max)
 		return (NULL);
-
-	ar = malloc(sizeof(*ar) * ((max - min) + 1));
-	if (ar == NULL)
-
-		return (NULL)
-
-			for (i = 0; min <= max; i++, min++)
-				ar[i] = min;
-	return (ar);
+	p = malloc(((max - min) + 1) * sizeof(int));
+	if (p == NULL)
+		return (NULL);
+	i = 0;
+	while (min <= max)
+		p[i++] = min++;
+	return (p);
 }
