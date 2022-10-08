167e4f3b3a5fbeb81efecad1e5362ead7181a6e4
KariukiD <kariukindiho@gmail.com>
2022-10-08 04:44:12 -0700

    pointers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	void print_diagsums(int *a, int size)
+	int i;
+	int diagO = 0;
+	int diagT = 0;
 
-		for (i = 0; i < (size * size); i++)
-		{
-			if (i % (size + 1) == 0)
-				sum1 += *(a + i);
-			if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-				sum2 += *(a + i);
-		}
-	printf("%d, %d\n", sum1, sum2);
+	for (i = 0; i < size; i++)
+	{
+		diagO += a[(i * size) + i];
+		diagT += a[(size - 1) + ((size - 1) * i)];
+	}
+	printf("%d, %d\n", diagO, diagT);
 }
