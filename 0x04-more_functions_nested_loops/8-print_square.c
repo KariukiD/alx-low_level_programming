17982b63d2ac854fea23474d97035dd164654ab9
KariukiD <kariukindiho@gmail.com>
2022-10-08 05:19:35 -0700

    nested loop

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square -prints a square
+ * @size: print size
+ * @size: print size
+ */
+void print_square(int size)
+{
+	int i;
+	int k;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (k = 0; k < size; k++)
+			{
+				_putchar (35);
+			}
+			_putchar ('\n');
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
