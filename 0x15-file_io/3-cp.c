b5b74816afe13d07effbc9ef52a07939417cf261
KariukiD <kariukindiho@gmail.com>
2022-11-02 12:56:05 -0700

    file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ void error_file(int file_from, int file_to, char *argv[])
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);\
+		exit(98);
 	}
 	if (file_to == -1)
 	{
